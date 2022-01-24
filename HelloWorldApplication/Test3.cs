92798448c003114f3231f2661e16ece01173f479
xinyusun1997528 <894579383@qq.com>
2022-01-23 19:12:32 -0500

    New file added

---
+++ HelloWorldApplication/Test3.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorApplication
+{
+    public class Test3
+    {
+        private int length;
+        private int width;
+        public Test3(int l, int w)
+        {
+            length = l;
+            width = w;
+            
+        }
+
+        public int getArea()
+        {
+            return length * width;
+        }
+
+        public int Length
+        {
+            get => length;
+            set => length = value;
+        }
+    }
+}
